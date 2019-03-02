be31f274162db3c6179dfebc837454f00263f6f4
Mohit Doshi <34958877+Mohit-Doshi@users.noreply.github.com>
2019-03-02 15:45:13 -0800

    Add files via upload

---
+++ Hacktechproj/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Hacktechproj
+//
+//  Created by Mohit on 02/03/19.
+//  Copyright © 2019 Mohit D. All rights reserved.
+//
+
+import UIKit
+import SCSDKLoginKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func snapchatLoginAction(_ sender: UIButton) {
+        SCSDKLoginClient.login(from: self) { success, error in
+            if let error = error {
+                // An error occurred during the login process
+                print(error.localizedDescription)
+            } else {
+                // The login was a success! This user is now
+                // authenticated with Snapchat!
+                print("Voila!")
+            }
+        }
+    }
+    
+    
+}
+
